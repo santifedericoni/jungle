6704f1165289dbe2bcb426612eb45c84110348f0
Santiago <santiago.federiconi@gmail.com>
2020-05-05 21:54:03 +0000

    add sales and cateogies controllers

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+  user = ENV['ADMIN_USER']
+  pass = ENV['ADMIN_PASS']
+  http_basic_authenticate_with name: user, password: pass
   def show
+    @product_count = Product.count()
+    @category_count = Category.count()
+    @order_count = Order.count()
+    @order_revenue = Order.sum(:total_cents)
   end
 end
---
+++ app/controllers/admin/sales_controller.rb
@@ -0,0 +1,41 @@
+class Admin::SalesController < ApplicationController
+  user = ENV['ADMIN_USER']
+  pass = ENV['ADMIN_PASS']
+  http_basic_authenticate_with name: user, password: pass
+
+  def index
+    @sales = Sale.all.order(:starts_on, :ends_on)
+  end
+
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+
+    if @sale.save
+      redirect_to [:admin, :sales], notice: 'Sale created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @sale = Sale.find params[:id]
+    @sale.destroy
+    redirect_to [:admin, :sales], notice: 'Sale deleted!'
+  end
+
+  private
+
+    def sale_params
+      params.require(:sale).permit(
+        :name,
+        :starts_on,
+        :ends_on,
+        :percent_off
+      )
+    end
+
+end
